57e4a98fd1950211ff3c0fb4dcc1f8fd8e4d6012
Ethan Megill <megill1352@gmail.com>
2017-06-01 13:46:52 -0400

    input box change location

--- public/JS/yelpApiCalls.js
+++ public/JS/yelpApiCalls.js
@@ -16,14 +16,16 @@ class App extends React.Component {
 	render() {
 		return(
 			<div>
-				<div className="yelpResults">
-					<YelpResults results={this.state.businesses} />
-				</div>
-
+				
 				<div className="data">
 					<input className="food" onChange={this.updateTerm} className="input" type="text" placeholder="Food" />
 					<input className="location" onChange={this.updateLocation} className="input" type="text" placeholder="location" />
 					<button onClick={this.yelpApiCall}>Search</button>
+
+					<div className="yelpResults">
+						<YelpResults results={this.state.businesses} />
+					</div>
+
 				</div>
 			</div>
 		)
@@ -65,16 +67,8 @@ function YelpResults(props) {
 								<div className="yelp_address">{result.location.address}</div>
 								<div className="yelp_location">{result.location.city}</div>
 								<img className="yelp_stars" src={result.rating_img_url_small} alt="" />
-								<button className="yelp_button" onClick="myFunction()">Favorite</button>
+								<button className="yelp_button" onClick={addToFavorites}>Add To Favorites</button>
 								<div className="clear"></div>
-
-							<img src={result.image_url} alt="" />
-							<div>{result.name}</div>
-							<div>{result.display_phone}</div>
-							<div>{result.location.address}</div>
-							<div>{result.location.city}</div>
-							<img src={result.rating_img_url_small} alt="" />
-							<button className="yelp_button" onClick={addToFavorites}>Add To Favorites</button>
 
 						</div>
 
