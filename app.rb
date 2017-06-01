d0a2fc024c727ff491b8d4951f90ae56d48bf2a4
Ian Zolitor <ianzolitor@gmail.com>
2017-06-01 10:50:33 -0400

    signup and signin close #4

--- app.rb
+++ app.rb
@@ -7,6 +7,34 @@ set :sessions, true
 require './Models'
 
 
+before do
+  @current_user= User.find(session[:user_id]) if session[:user_id]
+
+end
+
+get '/home' do
+  erb :home
+
+end
+
 get '/' do
   erb :index
+end
+
+post '/' do
+  User.create(username: params[:username],password: params[:password])
+  user = User.where(username: params[:username]).first
+  session[:user_id] = user.id
+  redirect '/home'
+
+end
+
+post '/signin' do
+  @user = User.where(username: params[:username]).first
+      if @user.password == params[:password]
+        session[:user_id] = @user.id
+        redirect '/home'
+      else
+        redirect '/'
+      end
 end
